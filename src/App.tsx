9991204f1530fb524d50abb1a766b84ba225bcdf
sanalashari03 <sana.lashari03gmail.com>
2025-10-07 22:24:18 +0500

    Fixed Experience import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Experience from "./components/experience";
+
 
 
 
