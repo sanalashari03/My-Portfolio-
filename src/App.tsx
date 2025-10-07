4dae79ec945f7ac16a1bdaf060eef0248b7f9a97
sanalashari03 <sana.lashari03gmail.com>
2025-10-07 22:29:05 +0500

    Fixed Experience import path (case-sensitive)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Experience from "./components/Experience";
+import Experience from "./components/experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
