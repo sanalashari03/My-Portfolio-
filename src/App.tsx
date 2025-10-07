740d96090fb8ef66849b942105edce394f5091b4
sanalashari03 <sana.lashari03gmail.com>
2025-10-07 23:24:56 +0500

    Fix import casing for Experience component to resolve build error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Experience from "./components/experience";
+import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
