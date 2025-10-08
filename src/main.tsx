f41084af9b871911e05164ff2599a16613199d30
sanalashari03 <sana.lashari03gmail.com>
2025-10-08 14:36:47 +0500

    Fix: rename About.css for case-sensitive build

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/Navbar.css";
 import "./styles/Hero.css";
-import "./styles/About.css";
+import "./styles/About-temp.css";
 import "./styles/Projects.css";
 import "./styles/Contact.css";
 import "./styles/Footer.css";
